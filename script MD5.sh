4fb6b868e88d4880294e3fcf92187398f1acb99a
Sigurd Myhre <69972850+cessnas@users.noreply.github.com>
2022-03-06 19:02:29 +0100

    Delete script MD5.sh

--- script MD5.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-hashcat -a 3 -o outputPassord1Md5 /Hovedprosjekt-FSInnlandet/rockyou.hcmask Hovedprosjekt-FSInnlandet/passord1Md5.txt
-hashcat -a 3 -o outputVerdensdel1995!Md5 /Hovedprosjekt-FSInnlandet/rockyou.hcmask Hovedprosjekt-FSInnlandet/Verdensdel1995!Md5.txt
-hashcat -a 3 -o outputb!gCar-71Md5 /Hovedprosjekt-FSInnlandet/rockyou.hcmask Hovedprosjekt-FSInnlandet/b!gCar-71Md5.txt
